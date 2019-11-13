f5131ddcf9e41756bdee892f1e4b68660c8e335e
Bagus06 <Bagusfikrianto7@gmail.com>
2019-11-13 09:27:09 +0700

    DB up

--- db/miss.sql
+++ db/miss.sql
@@ -3,14 +3,12 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: 13 Nov 2019 pada 01.26
+-- Generation Time: 13 Nov 2019 pada 09.22
 -- Versi Server: 5.7.27-0ubuntu0.18.04.1
 -- PHP Version: 7.2.24-0ubuntu0.18.04.1
 
-SET SQL_MODE
-= "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone
-= "+00:00";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -28,12 +26,9 @@ SET time_zone
 -- Struktur dari tabel `config`
 --
 
-CREATE TABLE `config`
-(
-  `id` int
-(11) NOT NULL,
-  `name` varchar
-(50) NOT NULL,
+CREATE TABLE `config` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
   `value` mediumtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -41,10 +36,7 @@ CREATE TABLE `config`
 -- Dumping data untuk tabel `config`
 --
 
-INSERT INTO `config` (`
-id`,
-`name
-`, `value`) VALUES
+INSERT INTO `config` (`id`, `name`, `value`) VALUES
 (1, 'th_ajaran', '{\"th_ajaran\":\"3\"}');
 
 -- --------------------------------------------------------
@@ -53,37 +45,24 @@ id`,
 -- Struktur dari tabel `guru`
 --
 
-CREATE TABLE `guru`
-(
-  `id` int
-(11) NOT NULL,
-  `user_id` int
-(11) NOT NULL,
-  `nama` varchar
-(255) NOT NULL,
-  `kode` varchar
-(5) NOT NULL,
-  `gender` tinyint
-(1) NOT NULL DEFAULT '1',
-  `alamat` varchar
-(255) NOT NULL,
-  `hp` varchar
-(15) NOT NULL,
-  `photo` int
-(11) NOT NULL,
+CREATE TABLE `guru` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `kode` varchar(5) NOT NULL,
+  `gender` tinyint(1) NOT NULL DEFAULT '1',
+  `alamat` varchar(255) NOT NULL,
+  `hp` varchar(15) NOT NULL,
+  `photo` int(11) NOT NULL,
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON
-UPDATE CURRENT_TIMESTAMP
+  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `guru`
 --
 
-INSERT INTO `guru` (`
-id`,
-`user_id
-`, `nama`, `kode`, `gender`, `alamat`, `hp`, `photo`, `created`, `updated`) VALUES
+INSERT INTO `guru` (`id`, `user_id`, `nama`, `kode`, `gender`, `alamat`, `hp`, `photo`, `created`, `updated`) VALUES
 (3, 13, 'Indah Purnamasari', 'A', 0, 'Bangsri', '081', 0, '2019-10-26 11:39:45', '2019-10-26 11:39:45'),
 (4, 14, 'Triana', 'AZ', 0, '-', '089', 0, '2019-10-26 11:40:15', '2019-10-26 11:40:15');
 
@@ -93,20 +72,13 @@ id`,
 -- Struktur dari tabel `guru_has_mapel`
 --
 
-CREATE TABLE `guru_has_mapel`
-(
-  `id` int
-(11) NOT NULL,
-  `guru_id` int
-(11) NOT NULL,
-  `mapel_id` int
-(11) NOT NULL,
-  `kelas_id` int
-(11) NOT NULL,
-  `th_ajaran_id` int
-(11) NOT NULL,
-  `hari` int
-(11) NOT NULL,
+CREATE TABLE `guru_has_mapel` (
+  `id` int(11) NOT NULL,
+  `guru_id` int(11) NOT NULL,
+  `mapel_id` int(11) NOT NULL,
+  `kelas_id` int(11) NOT NULL,
+  `th_ajaran_id` int(11) NOT NULL,
+  `hari` int(11) NOT NULL,
   `jam_mulai` time NOT NULL,
   `jam_selesai` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -117,22 +89,16 @@ CREATE TABLE `guru_has_mapel`
 -- Struktur dari tabel `kelas`
 --
 
-CREATE TABLE `kelas`
-(
-  `id` int
-(11) NOT NULL,
-  `nama` varchar
-(255) NOT NULL
+CREATE TABLE `kelas` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `kelas`
 --
 
-INSERT INTO `kelas` (`
-id`,
-`nama
-`) VALUES
+INSERT INTO `kelas` (`id`, `nama`) VALUES
 (11, 'X AKL 1'),
 (12, 'X AKL 2'),
 (4, 'X BDP 1'),
@@ -173,22 +139,16 @@ id`,
 -- Struktur dari tabel `mapel`
 --
 
-CREATE TABLE `mapel`
-(
-  `id` int
-(11) NOT NULL,
-  `nama` varchar
-(255) NOT NULL
+CREATE TABLE `mapel` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `mapel`
 --
 
-INSERT INTO `mapel` (`
-id`,
-`nama
-`) VALUES
+INSERT INTO `mapel` (`id`, `nama`) VALUES
 (2, 'Pendidikan Agama Islam'),
 (3, 'Ppkn'),
 (4, 'Matematika');
@@ -199,16 +159,11 @@ id`,
 -- Struktur dari tabel `presensi`
 --
 
-CREATE TABLE `presensi`
-(
-  `id` int
-(11) NOT NULL,
-  `siswa_id` int
-(11) NOT NULL,
-  `kelas_id` int
-(11) NOT NULL,
-  `keterangan` int
-(11) NOT NULL,
+CREATE TABLE `presensi` (
+  `id` int(11) NOT NULL,
+  `siswa_id` int(11) NOT NULL,
+  `kelas_id` int(11) NOT NULL,
+  `keterangan` int(11) NOT NULL,
   `tanggal` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -216,10 +171,7 @@ CREATE TABLE `presensi`
 -- Dumping data untuk tabel `presensi`
 --
 
-INSERT INTO `presensi` (`
-id`,
-`siswa_id
-`, `kelas_id`, `keterangan`, `tanggal`) VALUES
+INSERT INTO `presensi` (`id`, `siswa_id`, `kelas_id`, `keterangan`, `tanggal`) VALUES
 (1, 6, 1, 1, '2019-11-13'),
 (2, 11, 1, 2, '2019-11-13'),
 (6, 12, 1, 1, '2019-11-13');
@@ -230,30 +182,18 @@ id`,
 -- Struktur dari tabel `siswa`
 --
 
-CREATE TABLE `siswa`
-(
-  `id` int
-(11) NOT NULL,
-  `user_id` int
-(11) DEFAULT NULL,
-  `kelas_id` int
-(11) NOT NULL,
-  `th_ajaran_id` int
-(11) NOT NULL,
-  `angkatan` int
-(11) NOT NULL,
-  `nama` varchar
-(255) NOT NULL,
-  `nis` varchar
-(20) NOT NULL,
-  `nisn` varchar
-(20) NOT NULL,
-  `gender` tinyint
-(1) NOT NULL,
-  `photo` varchar
-(255) NOT NULL,
-  `tmpt_lhr` varchar
-(255) NOT NULL,
+CREATE TABLE `siswa` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `kelas_id` int(11) NOT NULL,
+  `th_ajaran_id` int(11) NOT NULL,
+  `angkatan` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `nis` varchar(20) NOT NULL,
+  `nisn` varchar(20) NOT NULL,
+  `gender` tinyint(1) NOT NULL,
+  `photo` varchar(255) NOT NULL,
+  `tmpt_lhr` varchar(255) NOT NULL,
   `tgl_lhr` date NOT NULL,
   `alamat` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -262,13 +202,7 @@ CREATE TABLE `siswa`
 -- Dumping data untuk tabel `siswa`
 --
 
-INSERT INTO `siswa` (`
-id`,
-`user_id`,
-`kelas_id`,
-`th_ajaran_id`,
-`angkatan
-`, `nama`, `nis`, `nisn`, `gender`, `photo`, `tmpt_lhr`, `tgl_lhr`, `alamat`) VALUES
+INSERT INTO `siswa` (`id`, `user_id`, `kelas_id`, `th_ajaran_id`, `angkatan`, `nama`, `nis`, `nisn`, `gender`, `photo`, `tmpt_lhr`, `tgl_lhr`, `alamat`) VALUES
 (6, 11, 1, 1, 1, 'Bagus Prods', '1111', '1113', 1, '-', 'jepara', '2019-10-24', 'Dermolo'),
 (11, 15, 1, 2, 2, 'Bagusss', '1233', '1233', 1, 'a', 'Jepara', '0000-00-00', 'Dermolo'),
 (12, 16, 1, 3, 3, 'Annas', '112233', '332211', 1, '-', 'jepara', '2019-11-12', 'sd');
@@ -279,22 +213,16 @@ id`,
 -- Struktur dari tabel `th_ajaran`
 --
 
-CREATE TABLE `th_ajaran`
-(
-  `id` int
-(11) NOT NULL,
-  `title` varchar
-(255) NOT NULL
+CREATE TABLE `th_ajaran` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `th_ajaran`
 --
 
-INSERT INTO `th_ajaran` (`
-id`,
-`title
-`) VALUES
+INSERT INTO `th_ajaran` (`id`, `title`) VALUES
 (1, '2017-2018'),
 (2, '2018-2019'),
 (3, '2019-2020');
@@ -305,33 +233,22 @@ id`,
 -- Struktur dari tabel `user`
 --
 
-CREATE TABLE `user`
-(
-  `id` int
-(11) NOT NULL,
-  `username` varchar
-(255) NOT NULL,
-  `password` varchar
-(255) NOT NULL,
-  `email` varchar
-(255) NOT NULL,
-  `token` varchar
-(255) NOT NULL,
-  `active` tinyint
-(1) NOT NULL DEFAULT '0',
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '0',
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON
-UPDATE CURRENT_TIMESTAMP
+  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user`
-  (`id`, `username
-
-`, `password`, `email`, `token`, `active`, `created`, `updated`) VALUES
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `token`, `active`, `created`, `updated`) VALUES
 (1, 'root', '$2y$10$C3zyhmbvnu7vD0DS.xjJT.RMTOp4oTDEV9/7UFljdIz.bOBsX6NPG', 'iwan@gmail.com', '', 0, '2019-09-25 20:42:39', '2019-09-26 02:32:42'),
 (2, 'aw', '$2y$10$C3zyhmbvnu7vD0DS.xjJT.RMTOp4oTDEV9/7UFljdIz.bOBsX6NPG', 'iwansafr@gmail.com', '', 0, '2019-09-25 22:17:18', '2019-09-25 22:17:18'),
 (11, '1113', '$2y$10$mdyEO6VkQ8zwBTa2X7XTxetUuO.R7cpLeSHkhBNiaQbSfojMoX7si', '-', '', 0, '2019-10-24 22:19:51', '2019-10-24 22:20:33'),
@@ -346,24 +263,17 @@ INSERT INTO `user`
 -- Struktur dari tabel `user_has_role`
 --
 
-CREATE TABLE `user_has_role`
-(
-  `id` int
-(11) NOT NULL,
-  `user_id` int
-(11) NOT NULL,
-  `user_role_id` int
-(11) NOT NULL
+CREATE TABLE `user_has_role` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `user_role_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `user_has_role`
 --
 
-INSERT INTO `user_has_role` (`
-id`,
-`user_id
-`, `user_role_id`) VALUES
+INSERT INTO `user_has_role` (`id`, `user_id`, `user_role_id`) VALUES
 (3, 2, 3),
 (36, 1, 3),
 (37, 1, 1),
@@ -380,28 +290,20 @@ id`,
 -- Struktur dari tabel `user_role`
 --
 
-CREATE TABLE `user_role`
-(
-  `id` int
-(11) NOT NULL,
-  `title` varchar
-(255) NOT NULL,
+CREATE TABLE `user_role` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
   `description` text NOT NULL,
-  `level` int
-(11) NOT NULL,
+  `level` int(11) NOT NULL,
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON
-UPDATE CURRENT_TIMESTAMP
+  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `user_role`
 --
 
-INSERT INTO `user_role` (`
-id`,
-`title
-`, `description`, `level`, `created`, `updated`) VALUES
+INSERT INTO `user_role` (`id`, `title`, `description`, `level`, `created`, `updated`) VALUES
 (1, 'admin', 'user untuk admin', 1, '2019-09-25 18:53:20', '2019-09-25 18:53:20'),
 (2, 'petugas', 'user untuk petugas', 2, '2019-09-25 18:54:19', '2019-09-25 19:28:51'),
 (3, 'guru', 'akun untuk guru', 10, '2019-09-25 22:16:47', '2019-09-25 22:16:47'),
@@ -415,108 +317,82 @@ id`,
 -- Indexes for table `config`
 --
 ALTER TABLE `config`
-ADD PRIMARY KEY
-(`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `guru`
 --
 ALTER TABLE `guru`
-ADD PRIMARY KEY
-(`id`),
-ADD KEY `user_id`
-(`user_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `guru_has_mapel`
 --
 ALTER TABLE `guru_has_mapel`
-ADD PRIMARY KEY
-(`id`),
-ADD KEY `guru_id`
-(`guru_id`),
-ADD KEY `mapel_id`
-(`mapel_id`),
-ADD KEY `guru_has_mapel_ibfk_3`
-(`kelas_id`),
-ADD KEY `th_ajaran_id`
-(`th_ajaran_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `guru_id` (`guru_id`),
+  ADD KEY `mapel_id` (`mapel_id`),
+  ADD KEY `guru_has_mapel_ibfk_3` (`kelas_id`),
+  ADD KEY `th_ajaran_id` (`th_ajaran_id`);
 
 --
 -- Indexes for table `kelas`
 --
 ALTER TABLE `kelas`
-ADD PRIMARY KEY
-(`id`),
-ADD UNIQUE KEY `nama`
-(`nama`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nama` (`nama`);
 
 --
 -- Indexes for table `mapel`
 --
 ALTER TABLE `mapel`
-ADD PRIMARY KEY
-(`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `presensi`
 --
 ALTER TABLE `presensi`
-ADD PRIMARY KEY
-(`id`),
-ADD KEY `siswa_id`
-(`siswa_id`),
-ADD KEY `kelas_id`
-(`kelas_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `siswa_id` (`siswa_id`),
+  ADD KEY `kelas_id` (`kelas_id`);
 
 --
 -- Indexes for table `siswa`
 --
 ALTER TABLE `siswa`
-ADD PRIMARY KEY
-(`id`),
-ADD KEY `user_id`
-(`user_id`),
-ADD KEY `kelas_id`
-(`kelas_id`),
-ADD KEY `th_ajaran_id`
-(`th_ajaran_id`),
-ADD KEY `angkatan`
-(`angkatan`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `kelas_id` (`kelas_id`),
+  ADD KEY `th_ajaran_id` (`th_ajaran_id`),
+  ADD KEY `angkatan` (`angkatan`);
 
 --
 -- Indexes for table `th_ajaran`
 --
 ALTER TABLE `th_ajaran`
-ADD PRIMARY KEY
-(`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-ADD PRIMARY KEY
-(`id`),
-ADD UNIQUE KEY `username`
-(`username`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- Indexes for table `user_has_role`
 --
 ALTER TABLE `user_has_role`
-ADD PRIMARY KEY
-(`id`),
-ADD KEY `user_id`
-(`user_id`),
-ADD KEY `user_role_id`
-(`user_role_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `user_role_id` (`user_role_id`);
 
 --
 -- Indexes for table `user_role`
 --
 ALTER TABLE `user_role`
-ADD PRIMARY KEY
-(`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -526,68 +402,57 @@ ADD PRIMARY KEY
 -- AUTO_INCREMENT for table `config`
 --
 ALTER TABLE `config`
-  MODIFY `id` int
-(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `guru`
 --
 ALTER TABLE `guru`
-  MODIFY `id` int
-(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `guru_has_mapel`
 --
 ALTER TABLE `guru_has_mapel`
-  MODIFY `id` int
-(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `kelas`
 --
 ALTER TABLE `kelas`
-  MODIFY `id` int
-(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 --
 -- AUTO_INCREMENT for table `mapel`
 --
 ALTER TABLE `mapel`
-  MODIFY `id` int
-(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `presensi`
 --
 ALTER TABLE `presensi`
-  MODIFY `id` int
-(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `siswa`
 --
 ALTER TABLE `siswa`
-  MODIFY `id` int
-(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT for table `th_ajaran`
 --
 ALTER TABLE `th_ajaran`
-  MODIFY `id` int
-(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int
-(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 --
 -- AUTO_INCREMENT for table `user_has_role`
 --
 ALTER TABLE `user_has_role`
-  MODIFY `id` int
-(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 --
 -- AUTO_INCREMENT for table `user_role`
 --
 ALTER TABLE `user_role`
-  MODIFY `id` int
-(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
@@ -596,91 +461,39 @@ ALTER TABLE `user_role`
 -- Ketidakleluasaan untuk tabel `guru`
 --
 ALTER TABLE `guru`
-ADD CONSTRAINT `guru_ibfk_1` FOREIGN KEY
-(`user_id`) REFERENCES `user`
-(`id`) ON
-DELETE CASCADE ON
-UPDATE CASCADE;
+  ADD CONSTRAINT `guru_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `guru_has_mapel`
 --
 ALTER TABLE `guru_has_mapel`
-ADD CONSTRAINT `guru_has_mapel_ibfk_1` FOREIGN KEY
-(`guru_id`) REFERENCES `guru`
-(`id`) ON
-DELETE CASCADE ON
-UPDATE CASCADE,
-ADD CONSTRAINT `guru_has_mapel_ibfk_2` FOREIGN KEY
-(`mapel_id`) REFERENCES `mapel`
-(`id`) ON
-DELETE CASCADE ON
-UPDATE CASCADE,
-ADD CONSTRAINT `guru_has_mapel_ibfk_3` FOREIGN KEY
-(`kelas_id`) REFERENCES `kelas`
-(`id`) ON
-DELETE CASCADE ON
-UPDATE CASCADE,
-ADD CONSTRAINT `guru_has_mapel_ibfk_4` FOREIGN KEY
-(`th_ajaran_id`) REFERENCES `th_ajaran`
-(`id`) ON
-DELETE CASCADE ON
-UPDATE CASCADE;
+  ADD CONSTRAINT `guru_has_mapel_ibfk_1` FOREIGN KEY (`guru_id`) REFERENCES `guru` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `guru_has_mapel_ibfk_2` FOREIGN KEY (`mapel_id`) REFERENCES `mapel` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `guru_has_mapel_ibfk_3` FOREIGN KEY (`kelas_id`) REFERENCES `kelas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `guru_has_mapel_ibfk_4` FOREIGN KEY (`th_ajaran_id`) REFERENCES `th_ajaran` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `presensi`
 --
 ALTER TABLE `presensi`
-ADD CONSTRAINT `presensi_ibfk_1` FOREIGN KEY
-(`siswa_id`) REFERENCES `siswa`
-(`id`) ON
-DELETE CASCADE ON
-UPDATE CASCADE,
-ADD CONSTRAINT `presensi_ibfk_2` FOREIGN KEY
-(`kelas_id`) REFERENCES `kelas`
-(`id`) ON
-DELETE CASCADE ON
-UPDATE CASCADE;
+  ADD CONSTRAINT `presensi_ibfk_1` FOREIGN KEY (`siswa_id`) REFERENCES `siswa` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `presensi_ibfk_2` FOREIGN KEY (`kelas_id`) REFERENCES `kelas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `siswa`
 --
 ALTER TABLE `siswa`
-ADD CONSTRAINT `siswa_ibfk_1` FOREIGN KEY
-(`user_id`) REFERENCES `user`
-(`id`) ON
-DELETE CASCADE ON
-UPDATE CASCADE,
-ADD CONSTRAINT `siswa_ibfk_2` FOREIGN KEY
-(`kelas_id`) REFERENCES `kelas`
-(`id`) ON
-DELETE CASCADE ON
-UPDATE CASCADE,
-ADD CONSTRAINT `siswa_ibfk_3` FOREIGN KEY
-(`th_ajaran_id`) REFERENCES `th_ajaran`
-(`id`) ON
-DELETE CASCADE ON
-UPDATE CASCADE,
-ADD CONSTRAINT `siswa_ibfk_4` FOREIGN KEY
-(`angkatan`) REFERENCES `th_ajaran`
-(`id`) ON
-DELETE CASCADE ON
-UPDATE CASCADE;
+  ADD CONSTRAINT `siswa_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `siswa_ibfk_2` FOREIGN KEY (`kelas_id`) REFERENCES `kelas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `siswa_ibfk_3` FOREIGN KEY (`th_ajaran_id`) REFERENCES `th_ajaran` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `siswa_ibfk_4` FOREIGN KEY (`angkatan`) REFERENCES `th_ajaran` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `user_has_role`
 --
 ALTER TABLE `user_has_role`
-ADD CONSTRAINT `user_has_role_ibfk_1` FOREIGN KEY
-(`user_id`) REFERENCES `user`
-(`id`) ON
-DELETE CASCADE ON
-UPDATE CASCADE,
-ADD CONSTRAINT `user_has_role_ibfk_2` FOREIGN KEY
-(`user_role_id`) REFERENCES `user_role`
-(`id`) ON
-DELETE CASCADE ON
-UPDATE CASCADE;
+  ADD CONSTRAINT `user_has_role_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_has_role_ibfk_2` FOREIGN KEY (`user_role_id`) REFERENCES `user_role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
